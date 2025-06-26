f9738318fb9e9cb5fc00400e7e9b70a4a3231311
ozzzi <osinezator.666@gmail.com>
2025-06-26 17:01:11 +0300

    chore: use strict mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,28 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Model::automaticallyEagerLoadRelationships();
+        Model::shouldBeStrict();
+        Model::unguard();
+
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
+
+        Date::use(CarbonImmutable::class);
     }
 }
