eb34fade2d4c011091313603f7c846b8b43372a8
ozzzi <osinezator.666@gmail.com>
2025-07-01 16:37:50 +0300

    feature: change id type for hits

--- app/Services/Search/DTO/Hit.php
+++ app/Services/Search/DTO/Hit.php
@@ -7,13 +7,13 @@ namespace App\Services\Search\DTO;
 final class Hit
 {
     /**
-     * @param string $id
+     * @param string|int $id
      * @param array<int, array<string, mixed>> $document
      * @param float|null $score
      * @param string|null $highlight
      */
     public function __construct(
-        public readonly string $id,
+        public readonly string|int $id,
         public readonly array $document,
         public readonly ?float $score = null,
         public readonly ?string $highlight = null
