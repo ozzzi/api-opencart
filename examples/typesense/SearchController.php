551e7f8ada18f6182ddfc51a11c03014e3a56c28
ozzzi <osinezator.666@gmail.com>
2025-09-07 14:36:14 +0300

    feature: add examples for typesense

---
+++ examples/typesense/SearchController.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SearchRequest;
+use App\Services\Search\Search\SearchBuilder;
+use App\Services\Search\SearchManager;
+use Illuminate\Http\JsonResponse;
+
+final class SearchController extends Controller
+{
+    public function __construct(private readonly SearchManager $searchManager)
+    {
+    }
+
+    public function __invoke(SearchRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $searchBuilder = $this->buildSearch($data);
+
+        return new JsonResponse(data: $searchBuilder->result());
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    public function buildSearch(array $data): SearchBuilder
+    {
+        $searchBuilder = $this->searchManager->createSearchBuilder($data['index']);
+
+        $this->setQuery($searchBuilder, $data)
+            ->setFilters($searchBuilder, $data)
+            ->setSorts($searchBuilder, $data)
+            ->setPagination($searchBuilder, $data)
+            ->setOptions($searchBuilder, $data);
+
+        return $searchBuilder;
+    }
+
+    /**
+     * @param array<string, string> $data
+     */
+    private function setQuery(SearchBuilder $searchBuilder, array $data): self
+    {
+        $searchBuilder->query($data['query']);
+
+        return $this;
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    private function setFilters(SearchBuilder $searchBuilder, array $data): self
+    {
+        $filters = $data['filters'] ?? [];
+
+        foreach ($filters as $filter) {
+            $searchBuilder->addFilter(
+                attribute: $filter['attribute'],
+                value: $filter['value'],
+                operator: $filter['operator'] ?? '='
+            );
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    private function setSorts(SearchBuilder $searchBuilder, array $data): self
+    {
+        $sorts = $data['sorts'] ?? [];
+
+        foreach ($sorts as $sort) {
+            $searchBuilder->addSort(
+                attribute: $sort['attribute'],
+                direction: $sort['direction'] ?? 'asc'
+            );
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<string, int> $data
+     */
+    private function setPagination(SearchBuilder $searchBuilder, array $data): self
+    {
+        if (isset($data['limit'])) {
+            $searchBuilder->limit($data['limit']);
+        }
+
+        if (isset($data['offset'])) {
+            $searchBuilder->offset($data['offset']);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    private function setOptions(SearchBuilder $searchBuilder, array $data): self
+    {
+        $excludeFields = $data['exclude_fields'] ?? [];
+        $excludeFields = implode(',', array_merge($excludeFields, ['embedding']));
+
+        $distanceThreshold = config('services.search.distance_threshold');
+
+        $searchBuilder->options([
+            'vector_query' => "embedding:([], distance_threshold:$distanceThreshold)",
+            'rerank_hybrid_matches' => true,
+            'exclude_fields' => $excludeFields,
+            'stopwords' => 'stopwords_cyrillic',
+        ]);
+
+        return $this;
+    }
+}
