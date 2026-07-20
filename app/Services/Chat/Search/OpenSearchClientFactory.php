2eeb1eac0c4bdc37bf5b2ca2c93a9a9e887511e8
ozzzi <osinezator.666@gmail.com>
2026-07-20 18:11:50 +0300

    feature: fix Opensearch client builder

--- app/Services/Chat/Search/OpenSearchClientFactory.php
+++ app/Services/Chat/Search/OpenSearchClientFactory.php
@@ -4,33 +4,22 @@ declare(strict_types=1);
 
 namespace App\Services\Chat\Search;
 
-use Illuminate\Support\Str;
 use OpenSearch\Client;
-use OpenSearch\ClientBuilder;
+use OpenSearch\GuzzleClientFactory;
 
 final class OpenSearchClientFactory
 {
     public static function make(): Client
     {
-        /** @var array{host:string,port:int,user:string,password:string,ssl:bool,ssl_verification:bool,debug:bool} $config */
-        $config = config('opensearch');
-
-        $host = Str::startsWith($config['host'], ['http://', 'https://'])
-            ? $config['host']
-            : ($config['ssl'] ? 'https' : 'http') . '://' . $config['host'];
-
-        $builder = ClientBuilder::create()
-            ->setHosts(["{$host}:{$config['port']}"])
-            ->setRetries(1);
-
-        if ($config['user'] !== '' && $config['password'] !== '') {
-            $builder->setBasicAuthentication($config['user'], $config['password']);
-        }
-
-        if ($config['ssl'] && ! $config['ssl_verification']) {
-            $builder->setSSLVerification(false);
-        }
-
-        return $builder->build();
+        return (new GuzzleClientFactory())
+            ->create([
+                'base_uri' => sprintf(
+                    '%s:%s',
+                    config('services.search.host'),
+                    config('services.search.port')
+                ),
+                'auth' => [config('services.search.user'), config('services.search.key')],
+                'verify' => config('services.search.ssl'),
+            ]);
     }
 }
