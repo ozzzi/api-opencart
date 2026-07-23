c53d449a4d5e6c0fc794e646682e5492e31c8caf
ozzzi <osinezator.666@gmail.com>
2026-07-23 12:06:34 +0300

    feature: change horizon guard

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Models\Bot\AdminUser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 final class HorizonServiceProvider extends HorizonApplicationServiceProvider
@@ -13,6 +16,12 @@ final class HorizonServiceProvider extends HorizonApplicationServiceProvider
     public function boot(): void
     {
         parent::boot();
+
+        Horizon::auth(static function (Request $request): bool {
+            $user = Auth::guard('admin')->user();
+
+            return $user !== null && Gate::forUser($user)->allows('viewHorizon');
+        });
     }
 
     /**
